93cd56d71d77ec37c23bb1984d40b6f389348999
danieltwh <71161486+danieltwh@users.noreply.github.com>
2022-12-06 23:00:16 -0500

    Remove wait time for RabbitMQ News consumer

---
+++ backend/worker_nlp_v6.py
@@ -0,0 +1,97 @@
+import pika
+import json
+import sys
+import os
+import time
+import traceback
+
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+import models, crud, schemas
+
+url = 'amqps://fzsatlsi:X6HeAHLC1mKDr2FZvSNIAWF9vg03riEs@woodpecker.rmq.cloudamqp.com/fzsatlsi'
+port = 	5672
+vhost = 'fzsatlsi'
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def main():
+    # connection = pika.BlockingConnection(
+    #     pika.ConnectionParameters(host = "localhost")
+    # )
+
+    connection = pika.BlockingConnection(
+        # pika.ConnectionParameters(url=url, port=port, vhost=vhost)
+        pika.URLParameters(url)
+    )
+
+
+    channel = connection.channel()
+    channel.exchange_declare(
+        exchange = "nlp_pred",
+        exchange_type = "direct"
+    )
+    channel.queue_declare(queue = "raw_news", durable = True)
+    channel.basic_qos(prefetch_count = 1)
+
+    method_frame,  header_frame, body = channel.basic_get(queue = "raw_news")
+
+    db = next(get_db())
+
+    while method_frame != None and method_frame.NAME == "Basic.GetOk":
+        
+        try:
+            raw_news = json.loads(body)
+            print(f"""################
+                Recevied Message: {raw_news}
+                ################
+                """)
+
+            # stock_ticker = raw_news['stock_ticker']
+            # stock = crud.get_stock(db, stock_ticker)
+            # if stock is None:
+            #     crud.create_stock(stock_ticker)
+
+            news_data = schemas.StockNewsCreate(
+                title = raw_news['title'],
+                date = raw_news['date'],
+                source = raw_news['source'],
+                url = raw_news['url'],
+                sentiment = raw_news['sentiment'],
+                match_score = raw_news['match_score'],
+                stock_ticker = raw_news['stock_ticker'],
+            )
+            db_news, db_stock_news = crud.create_stock_news(db, news_data)
+            
+            if db_news != None and db_stock_news != None:
+                print("Done")
+                channel.basic_ack(delivery_tag = method_frame.delivery_tag)
+            else: 
+                print('Failed')
+
+
+            
+        except Exception as e:
+            # print(e)
+            traceback.print_exc()
+        finally:
+            # time.sleep(5)
+            method_frame,  header_frame, body = channel.basic_get(queue = "raw_news")    
+
+    connection.close()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+    
